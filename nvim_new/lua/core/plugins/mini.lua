585295d5f95a5023987a8130e5003c02572030fd
aloysiogl <aloysiogl@gmail.com>
2023-08-23 22:09:38 +0200

    feat: surround support

--- nvim_new/lua/core/plugins/mini.lua
+++ nvim_new/lua/core/plugins/mini.lua
@@ -6,6 +6,20 @@ return {
       require("mini.ai").setup()
       require("mini.pairs").setup()
       require("mini.basics").setup()
+      require("mini.surround").setup {
+        mappings = {
+          add = 'za',      -- Add surrounding in Normal and Visual modes
+          delete = 'zd',   -- Delete surrounding
+          find = 'zf',     -- Find surrounding (to the right)
+          find_left = 'zF', -- Find surrounding (to the left)
+          highlight = 'zh', -- Highlight surrounding
+          replace = 'zr',  -- Replace surrounding
+          update_n_lines = 'zn', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      }
     end
   },
 }

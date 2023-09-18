d1950a2121995034bbf617dc80480ecae30ba4fa
aloysiogl <aloysiogl@gmail.com>
2023-09-18 17:02:00 +0200

    add: share system's clipboard

--- nvim_new/lua/core/config/options.lua
+++ nvim_new/lua/core/config/options.lua
@@ -1,16 +1,17 @@
 local opt = vim.opt
 
-opt.number = true         -- Line numbers
-opt.wrap = true           -- Line wrap
-opt.relativenumber = true -- Relative line numbers
-opt.hls = true            -- Highlight after search
-opt.expandtab = true      -- Use spaces instead of tabs
-opt.tabstop = 2           -- Number of spaces tabs count for
-opt.shiftround = true     -- Round indent
-opt.shiftwidth = 2        -- Size of an indent
-opt.incsearch = true      -- Search as characters are entered
+opt.number = true             -- Line numbers
+opt.wrap = true               -- Line wrap
+opt.relativenumber = true     -- Relative line numbers
+opt.hls = true                -- Highlight after search
+opt.expandtab = true          -- Use spaces instead of tabs
+opt.tabstop = 2               -- Number of spaces tabs count for
+opt.shiftround = true         -- Round indent
+opt.shiftwidth = 2            -- Size of an indent
+opt.incsearch = true          -- Search as characters are entered
 opt.grepprg = "rg --vimgrep"
-opt.scrolloff = 10        -- Lines on the border
+opt.scrolloff = 10            -- Lines on the border
+opt.clipboard = "unnamedplus" -- Use system clipboard
 
 -- netrw do not show ./ or ../
 vim.g.netrw_list_hide = "^\\./$,^\\../$"
@@ -19,3 +20,4 @@ vim.g.netrw_hide = 1
 -- bufstop
 vim.g.BufstopDismissKey = "q"
 vim.g.BufstopAutoSpeedToggle = 1
+

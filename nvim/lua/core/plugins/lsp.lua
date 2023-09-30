c5bb0723b7604fc1b8e9215cff499f2b224734fa
aloysiogl <aloysiogl@gmail.com>
2023-09-30 13:26:34 +0200

    add: diagnostics in lspconfig

--- nvim/lua/core/plugins/lsp.lua
+++ nvim/lua/core/plugins/lsp.lua
@@ -115,22 +115,33 @@ return {
           end
 
           vim.keymap.set({ 'n', 'x' }, '<leader>lf', function()
-            vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+            vim.lsp.buf.format({ async = true, timeout_ms = 10000 })
           end, generate_opts('LSP format'))
 
-          vim.keymap.set({ 'n' }, '<leader>lh', function()
-            vim.lsp.buf.hover()
-          end, generate_opts('LSP hover'))
+          vim.keymap.set({ 'n' }, '<leader>lwl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, generate_opts('LSP list_workspace_folders'))
 
-          vim.keymap.set({ 'n' }, '<leader>la', function()
-            vim.lsp.buf.code_action()
-          end, generate_opts('LSP action'))
+          vim.keymap.set({ 'n' }, '<leader>lh', vim.lsp.buf.hover, generate_opts('LSP hover'))
+          vim.keymap.set({ 'n' }, '<leader>la', vim.lsp.buf.code_action, generate_opts('LSP action'))
+          vim.keymap.set({ 'n' }, '<leader>lr', vim.lsp.buf.rename, generate_opts('LSP rename'))
+          vim.keymap.set({ 'n' }, '<leader>ld', vim.lsp.buf.definition, generate_opts('LSP definition'))
+          vim.keymap.set({ 'n' }, '<leader>lD', vim.lsp.buf.declaration, generate_opts('LSP declaration'))
+          vim.keymap.set({ 'n' }, '<leader>li', vim.lsp.buf.implementation, generate_opts('LSP implementation'))
+          vim.keymap.set({ 'n' }, '<leader>ls', vim.lsp.buf.signature_help, generate_opts('LSP signature_help'))
+          vim.keymap.set({ 'n' }, '<leader>lwa', vim.lsp.buf.add_workspace_folder,
+            generate_opts('LSP add_workspace_folder'))
+          vim.keymap.set({ 'n' }, '<leader>lwr', vim.lsp.buf.remove_workspace_folder,
+            generate_opts('LSP remove_workspace_folder'))
+          vim.keymap.set({ 'n' }, '<leader>le', vim.lsp.buf.references, generate_opts('LSP references'))
+          vim.keymap.set({ 'n' }, '<leader>lD', vim.lsp.buf.type_definition, generate_opts('LSP type_definition'))
 
-          vim.keymap.set({ 'n' }, '<leader>lr', function()
-            vim.lsp.buf.rename()
-          end, generate_opts('LSP rename'))
+          -- Disgnostics
+          vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, { desc = 'Diagnostics open_float'})
+          vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, { desc = 'Diagnostics goto_prev'})
+          vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, { desc = 'Diagnostics goto_next'})
+          vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Diagnostics setloclist'})
         end)
-
 
         -- (Optional) Configure lua language server for neovim
         require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())

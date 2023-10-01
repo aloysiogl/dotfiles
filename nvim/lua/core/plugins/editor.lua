900f26c0a3ef9171f2271e4ff97f0832a832c05c
aloysiogl <aloysiogl@gmail.com>
2023-10-01 14:26:44 +0200

    add: noice

--- nvim/lua/core/plugins/editor.lua
+++ nvim/lua/core/plugins/editor.lua
@@ -8,17 +8,17 @@ return {
       -- load the colorscheme here
       vim.cmd([[colorscheme catppuccin]])
     end,
-  
+
   },
- -- {
- --    "folke/tokyonight.nvim",
- --    lazy = false, -- make sure we load this during startup if it is your main colorscheme
- --    priority = 1000, -- make sure to load this before all the other start plugins
- --    config = function()
- --      -- load the colorscheme here
- --      vim.cmd([[colorscheme tokyonight]])
- --    end,
- --  },
+  -- {
+  --    "folke/tokyonight.nvim",
+  --    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --    priority = 1000, -- make sure to load this before all the other start plugins
+  --    config = function()
+  --      -- load the colorscheme here
+  --      vim.cmd([[colorscheme tokyonight]])
+  --    end,
+  --  },
   {
     "terrortylor/nvim-comment",
     config = function()
@@ -55,7 +55,37 @@ return {
       "nvim-lua/plenary.nvim"
     }
   },
+  -- lazy.nvim
   {
-    "nvim-tree/nvim-tree.lua"
-  },
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        hover = {
+          enabled = false, -- disables hover support
+        },
+        signature = {
+          enabled = false, -- disables hover support
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true,         -- use a classic bottom cmdline for search
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+      },
+      messages = {
+        enabled = false,
+      }
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  }
 }

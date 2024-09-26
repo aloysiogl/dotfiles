c7f00301491a966ec2e16d2a548518a980cb669c
aloysiogl <aloysiogl@gmail.com>
2024-09-26 11:00:44 +0200

    refact: nvim nagivation

--- nvim/lua/core/plugins/navigation.lua
+++ nvim/lua/core/plugins/navigation.lua
@@ -13,11 +13,11 @@ return {
           require("telescope").load_extension "file_browser"
         end,
       },
-    }
+    },
   },
   {
     "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
   },
   {
     "christoomey/vim-tmux-navigator",

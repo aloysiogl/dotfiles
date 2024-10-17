b5c541e411a9d10648ee30139b83dbdd85db5dbc
aloysiogl <aloysiogl@gmail.com>
2024-10-17 13:22:30 +0200

    feat: nvim autosave

--- nvim/lua/core/plugins/editor.lua
+++ nvim/lua/core/plugins/editor.lua
@@ -10,6 +10,17 @@ return {
     end,
 
   },
+  {
+    "okuuva/auto-save.nvim",
+    version = '^1.0.0', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+    -- cmd = "ASToggle", -- optional for lazy loading on command
+    -- event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    event = { "InsertLeave" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
+  },
   -- {
   --    "folke/tokyonight.nvim",
   --    lazy = false, -- make sure we load this during startup if it is your main colorscheme

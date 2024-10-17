553c0707c42e9c16f6b141c03e8b410a8725651d
aloysiogl <aloysiogl@gmail.com>
2024-10-17 15:36:20 +0200

    feat: new packages with nix

--- nix/config.nix
+++ nix/config.nix
@@ -5,11 +5,20 @@
     myPackages = pkgs.buildEnv {
       name = "my-packages";
       paths = [
+        # Programs
         pkgs.bat
         pkgs.neovim
         pkgs.vim
         pkgs.zoxide
         pkgs.lazygit
+        pkgs.nvimpager
+        pkgs.zotero
+
+        # Git
+        pkgs.delta
+
+        # Node
+        pkgs.nodejs_22
 
         # Nvim
         tree-sitter

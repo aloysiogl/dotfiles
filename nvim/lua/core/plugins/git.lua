59d2c727274c089e59b7089e54dfabd3bd353a19
aloysiogl <aloysiogl@gmail.com>
2024-10-25 16:56:21 +0200

    feat: new git integration which uses vim directly

--- nvim/lua/core/plugins/git.lua
+++ nvim/lua/core/plugins/git.lua
@@ -9,6 +9,19 @@ return {
   { "tpope/vim-fugitive" },
   { "sindrets/diffview.nvim" },
   {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",            -- optional
+      "echasnovski/mini.pick",       -- optional
+    },
+    config = true
+  },
+  {
     "lewis6991/gitsigns.nvim",
     config = function()
       require('gitsigns').setup {

56215041dc0186468f22093910a74cc430b35212
aloysiogl <aloysiogl@gmail.com>
2025-02-17 14:10:46 +0100

    feat: better shortcut to zoom in nvim

--- nvim/lua/core/config/terminal.lua
+++ nvim/lua/core/config/terminal.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>t", function()
+vim.keymap.set("n", "<leader>z", function()
     vim.cmd('!tmux resize-pane -Z && tmux refresh-client -S')
     vim.cmd('redraw!')
 end, { silent = true })

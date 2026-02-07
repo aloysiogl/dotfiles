2b2d4276ec5439e889de1ac285436d1edb446cd7
aloysiogl <aloysiogl@gmail.com>
2026-02-07 17:15:37 -0300

    feat: add additional regex patterns for file movement

--- scripts/movematching.py
+++ scripts/movematching.py
@@ -14,6 +14,8 @@ def move_file_to_folder(file_path: str, folder_path: str):
 
 def main():
     regexes_to_paths = {
+        r"COMPTEDEDEPOT_08941248671_\d{8}.pdf": "/home/aloysio/gdrive/pessoal/financeiro/bancos/lcl/documents/input",
+        r"COMPTEDEDEPOTS_08941248639_\d{8}.pdf": "/home/aloysio/gdrive/pessoal/financeiro/bancos/lcl/documents/input",
         r"\d{4}_\d{2}_BP_(janvier|fevrier|mars|avril|mai|juin|juillet|aout|septembre|octobre|novembre|decembre)": "/home/aloysio/gdrive/pessoal/financeiro/rendimentos/2025/x",
         r"sfr-facture-\d{6}.pdf": "/home/aloysio/gdrive/pessoal/financeiro/franca/internet/2025",
     }

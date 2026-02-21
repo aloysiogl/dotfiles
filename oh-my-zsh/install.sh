fa385fd1e287c57eefb896de280085897b04f4c7
aloysiogl <aloysiogl@gmail.com>
2026-02-21 15:44:49 -0300

    feat: update shortcuts and implement new features in my vi-mode in zsh

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -2,6 +2,8 @@
 export ZSH=~/.oh-my-zsh
 export ZSH_CUSTOM=~/.oh-my-zsh/custom
 export FZF_PATH=~/.fzf
+ZSH_VI_MODE_ORIGIN="https://github.com/aloysiogl/zsh-vi-mode.git"
+ZSH_VI_MODE_UPSTREAM="https://github.com/jeffreytse/zsh-vi-mode"
 
 if [ ! -d $ZSH ]; then
   git clone https://github.com/robbyrussell/oh-my-zsh.git $ZSH
@@ -33,8 +35,30 @@ if [ ! -d $ZSH_CUSTOM/plugins/zsh-output-highlighting ]; then
 fi
 
 if [ ! -d $ZSH_CUSTOM/plugins/zsh-vi-mode ]; then
-  git clone https://github.com/jeffreytse/zsh-vi-mode \
-    $ZSH_CUSTOM/plugins/zsh-vi-mode
+  git clone "$ZSH_VI_MODE_ORIGIN" $ZSH_CUSTOM/plugins/zsh-vi-mode
+fi
+
+if [ -d $ZSH_CUSTOM/plugins/zsh-vi-mode/.git ]; then
+  cd $ZSH_CUSTOM/plugins/zsh-vi-mode
+
+  git remote set-url origin "$ZSH_VI_MODE_ORIGIN"
+  if ! git remote get-url upstream >/dev/null 2>&1; then
+    git remote add upstream "$ZSH_VI_MODE_UPSTREAM"
+  fi
+
+  git fetch origin
+  git fetch upstream
+
+  # Try to fast-forward to upstream
+  git checkout master >/dev/null 2>&1
+  if ! git merge --ff-only upstream/master >/dev/null 2>&1; then
+    echo "WARNING: Could not fast-forward to upstream/master. Manual merge may be required."
+  fi
+
+  # Warn if upstream does not contain local fork changes
+  if ! git merge-base --is-ancestor origin/master upstream/master; then
+    echo "WARNING: Upstream does not contain your fork's master changes."
+  fi
 fi
 
 ZSH_THEME_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k

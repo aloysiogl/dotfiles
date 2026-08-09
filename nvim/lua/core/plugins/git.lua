a9a47258575d22f32ac7ad1aa2c1275a4df09058
aloysiogl <aloysiogl@gmail.com>
2026-08-09 17:51:44 -0300

    Add GitHub PR reviewer to Neovim

--- nvim/lua/core/plugins/git.lua
+++ nvim/lua/core/plugins/git.lua
@@ -51,6 +51,84 @@ return {{
     },
     config = true
 }, {
+    "otavioschwanck/github-pr-reviewer.nvim",
+    opts = {
+        next_hunk_key = "<C-n>",
+        prev_hunk_key = "<C-S-n>",
+        mark_as_viewed_key = "<Tab>",
+        next_file_key = "<Plug>(PRNextFile)",
+        prev_file_key = "<Plug>(PRPrevFile)"
+    },
+    config = function(_, opts)
+        local reviewer = require("github-pr-reviewer")
+        reviewer.setup(opts)
+
+        local float_mode = "all"
+        local original_toggle_floats = reviewer.toggle_floats
+
+        local function close_float(field)
+            local win = reviewer[field]
+            if win and vim.api.nvim_win_is_valid(win) then
+                vim.api.nvim_win_close(win, true)
+            end
+            reviewer[field] = nil
+        end
+
+        local function close_summary_floats()
+            close_float("_float_win_general")
+            close_float("_float_win_buffer")
+        end
+
+        local function close_keymap_float()
+            close_float("_float_win_keymaps")
+        end
+
+        reviewer.toggle_floats = function()
+            if not vim.g.pr_review_number then
+                return
+            end
+
+            if float_mode == "all" then
+                float_mode = "summary"
+                close_keymap_float()
+                vim.notify("Showing PR file and change summary", vim.log.levels.INFO)
+            elseif float_mode == "summary" then
+                float_mode = "none"
+                reviewer.config.show_floats = false
+                close_summary_floats()
+                close_keymap_float()
+                vim.notify("PR floating windows hidden", vim.log.levels.INFO)
+            else
+                float_mode = "all"
+                reviewer.config.show_floats = false
+                original_toggle_floats()
+            end
+        end
+
+        vim.api.nvim_create_autocmd({"BufEnter", "CursorMoved"}, {
+            group = vim.api.nvim_create_augroup("GithubPrReviewerFloatMode", {
+                clear = true
+            }),
+            callback = function()
+                if float_mode == "summary" then
+                    close_keymap_float()
+                end
+            end
+        })
+    end,
+    keys = {
+        {
+            "<leader>p",
+            "<cmd>PRReviewMenu<cr>",
+            desc = "PR review menu"
+        }, {
+            "<leader>p",
+            ":<C-u>'<,'>PRSuggestChange<CR>",
+            mode = "v",
+            desc = "Suggest PR change"
+        }
+    }
+}, {
     "lewis6991/gitsigns.nvim",
     config = function()
         require('gitsigns').setup {
